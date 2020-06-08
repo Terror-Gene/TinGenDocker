86aae7d6740dbdd06378da7f848a5c565161c8b4
yongbi85 <chonchu85@hotmail.com>
2020-06-08 20:56:01 +0200

    Update start_tingen.sh

--- start_tingen.sh
+++ start_tingen.sh
@@ -3,4 +3,4 @@
 cd /tingen
 git pull
 
-python TinGen.py --headless --success "Custom Index loaded successfully" --add-non-nsw-files --upload-to-folder_id ${FOLDER_ID_TO_UPLOAD_TO} ${FOLDER_ID_TO_SCAN}
+python TinGen.py --headless --credentials ${CREDENTIALS_PATH} --token ${TOKEN_PATH} --index-file ${INDEX_FILE_PATH} --success "Custom Index loaded successfully" --add-non-nsw-files --upload-to-folder_id ${FOLDER_ID_TO_UPLOAD_TO} ${FOLDER_ID_TO_SCAN}
