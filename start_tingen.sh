94afb121177a1b6c9c98c49ceceeeaf499eab7ef
yongbi85 <chonchu85@hotmail.com>
2020-06-08 21:27:30 +0200

    Update start_tingen.sh

--- start_tingen.sh
+++ start_tingen.sh
@@ -3,4 +3,11 @@
 cd /tingen
 git pull
 
+if [ -n "${CREDENTIALS_PATH}" ]; then
+  echo "Credentials path set to ${CREDENTIALS_PATH}"
+else
+echo "YOU MUST SPECIFY CREDENTIALS_PATH ENIVRONMENT VARIABLE TO POINT TO YOUR credentials.json FILE" 
+exit 1
+fi
+
 python TinGen.py --headless --credentials ${CREDENTIALS_PATH} --token ${TOKEN_PATH} --index-file ${INDEX_FILE_PATH} --success "Custom Index loaded successfully" --add-non-nsw-files --upload-to-folder_id ${FOLDER_ID_TO_UPLOAD_TO} ${FOLDER_ID_TO_SCAN}
